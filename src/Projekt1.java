eca6c77fac67f6773a006f130f107432e7f33548
Kristina-Kondratiuk <kondrk4717913_aehit@students.vizja.pl>
2024-03-09 14:29:36 +0100

    Pierwszy zapis do repozytorium Lab2

--- src/Projekt1.java
+++
@@ -1,9 +0,0 @@
-package pl.pp;
-
-public class Projekt1 {
-
-    public static void main(String[] args) {
-
-        System.out.println("Witaj PP!");
-    }
-}
---
+++ src/mojaDrugaAplikacja.java
@@ -0,0 +1,18 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+public class mojaDrugaAplikacja {
+    public static void main(String[] args) {
+
+        int x=10;
+        System.out.println("x = " + x);
+
+        var result = x * 2;
+        System.out.println("x * 2 = " + result);
+
+        result = x * x;
+        System.out.println("x^2 = " + result);
+
+    }
+}
